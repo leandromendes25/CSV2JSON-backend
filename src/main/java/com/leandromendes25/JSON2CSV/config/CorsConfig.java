c5a0a52f220e9871cfa3c2476776dae3c466d653
leandromendes25 <leandromiendes@hotmail.com>
2026-04-29 18:10:29 -0300

    adicionando build online

--- src/main/java/com/leandromendes25/JSON2CSV/config/CorsConfig.java
+++ src/main/java/com/leandromendes25/JSON2CSV/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200")
+                        .allowedOrigins("http://localhost:4200", "https://csv-2-json-frontend.vercel.app")
                         .allowedMethods("*");
             }
         };
